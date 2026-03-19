d2a92e137166c37d6289b5d3d43d5e35ad44b2cc
tayabunn <tayabunnesa@gmail.com>
2026-03-20 00:35:11 +0600

    slides updated

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+ tailwind.config = {
+      theme: {
+        extend: {
+          fontFamily: {
+            display: ['Syne', 'sans-serif'],
+            body:    ['DM Sans', 'sans-serif'],
+          },
+          colors: {
+            brand: {
+              blue:   '#2563EB',
+              dark:   '#0A0A0F',
+              mid:    '#111118',
+              card:   '#16161E',
+              border: '#1E1E2A',
+            }
+          }
+        }
+      }
+    }
